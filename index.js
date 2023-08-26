a60a16d428e99beefb523714313b82c9dfe0700f
Rogelio <red1hhh@gmai.com>
2023-08-26 13:44:44 -0600

    add a landing page

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const fs = require("fs");
+const path = require("path");
 
 const app = express();
 app.use(express.json());
@@ -7,20 +8,23 @@ app.use(express.json());
 const cors = require("cors");
 app.use(cors());
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/", (req, resp) => {
-  const status = {
-    "STATUS:": "Running",
-  };
-  resp.send(status);
+  resp.render("./public/index.html");
 });
 
 app.get("/teams", (req, resp) => {
-  let rawdata = fs.readFileSync("./res/teams.json");
+  let rawdata = fs.readFileSync("./data/teams.json");
   let teams = JSON.parse(rawdata);
   resp.send(teams);
 });
 
-const PORT = process.env.PORT || 3000;
+app.get("/about", (req, res) => {
+  res.send("This is my about route..... ");
+});
+
+const PORT = 4000;
 app.listen(PORT, () => {
   console.log("Server listening on port", PORT);
 });
